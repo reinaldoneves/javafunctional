12a7a2f494715258e149d6419ae951134af5ae31
Reinaldo Neves <reinaldo_neves@hotmail.com>
2022-05-16 14:35:39 -0300

    Lambdas

---
+++ src/main/java/br/com/functional/finalsection/Lambdas.java
@@ -0,0 +1,31 @@
+package br.com.functional.finalsection;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class Lambdas {
+
+    public static void main(String[] args) {
+
+        // Lambdas when you have only one return statement
+        Function<String, String> upperCaseName = name -> name.toUpperCase();
+        // This is the same as (with method reference):
+        Function<String, String> upperCaseName2 = String::toUpperCase;
+
+        //Lambdas when you have to write some logic and not return something imediatlly
+        Function<String, String> upperCaseName3 = name -> {
+            if (name.isBlank()) throw new IllegalArgumentException("Name cannot be blank");
+            return name.toUpperCase();
+        };
+
+        BiFunction<String, Integer, String> upperCaseName4 =
+                (name, age) -> {
+                    if (name.isBlank()) throw new IllegalArgumentException("Name cannot be blank");
+                    System.out.println(age);
+                    return name.toUpperCase();
+                };
+
+        System.out.println(upperCaseName4.apply("Naldo", 30));
+
+    }
+}
